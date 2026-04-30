47015bafd4b5390240d42c1f904f8b3975c4d14f
Ning Wang <unidentifiedme26@gmail.com>
2026-04-30 18:38:20 +0800

    feat(claude-code): sync the new statusline look from nixfiles

--- dot_config/ccstatusline/ccsl-context-bar.ps1
+++ dot_config/ccstatusline/ccsl-context-bar.ps1
@@ -1,9 +1,15 @@
 $input | jq -r '
+  def fmt_tokens:
+    if . == null then "?"
+    elif . >= 1000000 then "\(. / 1000000 * 10 | round / 10)M"
+    elif . >= 1000 then "\(. / 1000 * 10 | round / 10)k"
+    else "\(.)" end;
   .context_window // {} |
   (.used_percentage // 0) as $pct |
+  (.context_window_size // null) as $size |
   10 as $width |
   (($pct / 100 * $width) | round) as $filled |
-  [range($width)] | map(
+  ([range($width)] | map(
     if . < $filled then
       if . == 0 then "\uEE03"
       elif . == ($width - 1) then "\uEE05"
@@ -13,5 +19,5 @@ $input | jq -r '
       elif . == ($width - 1) then "\uEE02"
       else "\uEE01" end
     end
-  ) | join("") + " \($pct)%"
+  ) | join("")) + " \($pct)% / \($size | fmt_tokens)"
 '
--- dot_config/ccstatusline/ccsl-model-alias.ps1
+++
@@ -1,8 +0,0 @@
-$input | jq -r '
-  .model // empty |
-  if type == "string" then . else (.display_name // .id // "") end |
-  split("/") | last |
-  sub("^claude-"; "") |
-  sub("-[0-9]{8}$"; "") |
-  sub("\\[1m\\]$"; "")
-'
